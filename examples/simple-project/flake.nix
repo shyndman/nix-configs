b088d3b08c5281b911129cb4cff789acd9543524
Scott Hyndman <scotty.hyndman@gmail.com>
2025-04-16 01:30:52 -0700

    Whitespace

--- examples/simple-project/flake.nix
+++ examples/simple-project/flake.nix
@@ -20,7 +20,7 @@
             python311
             nodejs_20
           ];
-          
+
           shellHook = ''
             echo "Welcome to the project development environment!"
             echo "May the blessings of Nixus be upon your code!"
@@ -32,22 +32,22 @@
           name = "simple-project";
           version = "0.1.0";
           src = ./.;
-          
+
           buildInputs = with pkgs; [
             python311
           ];
-          
+
           buildPhase = ''
             mkdir -p $out/bin
             cp ${./main.py} $out/bin/simple-project
             chmod +x $out/bin/simple-project
           '';
-          
+
           installPhase = ''
             # Nothing to do here as we did everything in buildPhase
           '';
         };
-        
+
         # App definition
         apps.default = {
           type = "app";
